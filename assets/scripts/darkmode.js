a145ac44c5d5f9b3aab0173c99021fa3c336705a
Dan Boots <thestereojackers@gmail.com>
2020-09-04 15:05:16 +0000

    local storage version of theme switcher coding complete, simple use case as is, simple toggle button with one base background colour switch, this can be themed out correctly however javascript works. Commiting now as hero section switcher button image fades and theme switcher all working time for a beer!

--- assets/scripts/darkmode.js
+++ assets/scripts/darkmode.js
@@ -1,46 +1,102 @@
-/* this code was adapted from the tutorial found here: https://www.youtube.com/watch?v=wodWDIdV9BY
-although to test my knowledge and understanding i rewrote it in jquery.
-*/
+// /* this code was adapted from the tutorial found here: https://www.youtube.com/watch?v=wodWDIdV9BY
+// although to test my knowledge and understanding i rewrote it in jquery.
+// */
+
+// $(document).ready(function () {
  
-let darkmode = localStorage.getItem('darkMode');
+// let darkmode = localStorage.getItem('darkMode');
+// const darkModeToggle = document.querySelector('#darkmodetoggle');
+
+
+// //check if dark mode is enabled, if disabled turn on.
+
+// const enableDarkMode = () =>{
+//     // add class dark mode to body.
+//     $('body').addClass('darkmode');
+//     // update darkMode in local storage
+//     localStorage.setItem('darkMode', 'enabled');
+// };
+
+// const disableDarkMode = () =>{
+//     // add class dark mode to body.
+//     $('body').removeClass('darkmode');
+//     // update darkMode in local storage
+//     localStorage.setItem('darkMode', null);
+// };
+
+// // check mode from previous. 
+
+// if (darkMode ==='enabled') {
+//     enableDarkMode();
+// }
+
+// // using if and else to enable and disable modes.
+
+// $('#darkModeToggle').click(function(){
+//     darkMode = localStorage.getItem('darkMode');
+//     if (darkMode !== 'enabled') {
+//         enableDarkMode();
+//     }
+// else {
+//     disableDarkMode();
+// }
+
+// });
+// })
+ 
+
+
+
+$(document).ready(function () {
+
+// alert('check1 check2.... test toggle now in console');
+
+
+let darkMode = localStorage.getItem('darkMode'); 
+
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
+//test on page load if dark mode enables..
 
-//check if dark mode is enabled, if disabled turn on.
+console.log(darkMode);
 
-const enableDarkMode = () =>{
-    // add class dark mode to body.
-    $('body').addClass('darkmode');
-    // update darkMode in local storage
-    localStorage.setItem('darkMode', 'enabled');
+//check if Dmode is enabled, if enabled turn it off if dis turn it on.
+
+const enableDarkMode = () => {
+//add class dark mode to body. 
+document.body.classList.add('darkmode');
+//and update dark mode in locale storage. 
+localStorage.setItem('darkMode', 'enabled');
 };
 
-const disableDarkMode = () =>{
-    // add class dark mode to body.
-    $('body').removeClass('darkmode');
-    // update darkMode in local storage
-    localStorage.setItem('darkMode', null);
+const disableDarkMode = () => {
+//remove class dark mode to body. 
+document.body.classList.remove('darkmode');
+//and update dark mode in locale storage. 
+localStorage.setItem('darkMode', null);
 };
 
-// check mode from previous. 
 
-if (darkMode ==='enabled') {
+//check for previous visit for dark mode enable or disable. Then test.
+
+if (darkMode === 'enabled') {
     enableDarkMode();
 }
 
-// using if and else to enable and disable modes.
+darkModeToggle.addEventListener('click',() => {
+// console.log('test click event listener');
 
-$('#darkModeToggle').click(function(){
-    darkMode = localStorage.getItem('darkMode');
-    if (darkMode !== 'enabled') {
-        enableDarkMode();
-    }
+//set up if statement 
+darkMode = localStorage.getItem('darkMode');
+if (darkMode !== 'enabled') {
+    enableDarkMode();
+    console.log(darkMode);
+}
 else {
     disableDarkMode();
+    console.log(darkMode);
 }
 
 });
 
-
-
-
+});
