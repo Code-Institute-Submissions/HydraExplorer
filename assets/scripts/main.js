93c599a390014b16773ca6a497d6a58cd571d6b7
Dan Boots <thestereojackers@gmail.com>
2020-09-12 09:56:22 +0000

    fix for JSON data 404 error in console, ammended url

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,44 +3,34 @@
 /*hardcoded JSON Data ... written by myself, i will use this as a replacement to user input data as  i dont know how to use databases yet, and will increase
 functionality to the members profile by using external API from Spotify to create a dynamic user profile page this can be found in scripts, users.JSON file */
 
-
 let ourRequest = new XMLHttpRequest();
 
-ourRequest.open('GET', '../assets/scripts/users.json');
-ourRequest.onload = function(){
-
-    if(ourRequest.status >= 200 && ourRequest.status <400) {
-        let data = JSON.parse(ourRequest.responseText);
-        console.log('A OK El Capitan!');
-        createHTML(data);
-    }
-    else{
-console.log('we connected to the server but it returned an error');
-
-    }
-    
+ourRequest.open("GET", "https://danielboots.github.io/HydraExplorer/assets/scripts/users.json");
+ourRequest.onload = function () {
+  if (ourRequest.status >= 200 && ourRequest.status < 400) {
+    let data = JSON.parse(ourRequest.responseText);
+    console.log("A OK El Capitan!");
+    createHTML(data);
+  } else {
+    console.log("we connected to the server but it returned an error");
+  }
 };
-ourRequest.onerror = function(){
-    console.log('connection error');
+ourRequest.onerror = function () {
+  console.log("connection error");
 };
 
 ourRequest.send();
 
 
-// Handlebars.js - template function 
+// Handlebars.js - template function
 
 function createHTML(hydraUserData) {
-     
+  let rawTemplate = document.getElementById("userCardTemplate").innerHTML;
+  let compiledTemplate = Handlebars.compile(rawTemplate);
+  let ourGeneratedHTML = compiledTemplate(hydraUserData);
 
-    let rawTemplate = document.getElementById('userCardTemplate').innerHTML;
-    let compiledTemplate = Handlebars.compile(rawTemplate);
-    let ourGeneratedHTML = compiledTemplate(hydraUserData);
-   
-   let userContainer = document.getElementById('user-card-container');
-   userContainer.innerHTML = ourGeneratedHTML;
+  let userContainer = document.getElementById("user-card-container");
+  userContainer.innerHTML = ourGeneratedHTML;
 
-   console.log(hydraUserData);
-
+  console.log(hydraUserData);
 }
-
-
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,131 +1,124 @@
 //Added in document.ready function to ensure that all html is loaded before these scripts run.
-//Jquery 
+//Jquery
 $(document).ready(function () {
+  // Pill button click event to show and hide selections.
 
+  $("#btn-all").click(function () {
+    $("#hero").removeClass().addClass("hero-hydra");
+    $(".hero-text p").empty();
+    $(".singers, .producers, .remixers, .consultancy ").fadeTo(1000, 1);
 
-    // Pill button click event to show and hide selections.
+    // Condensed down code to multiple selectors, keeping in below as evidence of previous code.
+    // $('.producers').show();
+    // $('.remixers').show();
+    // $('.consultancy').show();
+  });
 
-    $('#btn-all').click(function () {
-        $('#hero').removeClass().addClass('hero-hydra');
-        $('.hero-text p').empty()
-        $('.singers, .producers, .remixers, .consultancy ').fadeTo(1000, 1);
+  $("#btn-singers").click(function () {
+    $("#hero").removeClass().toggleClass("hero-singer");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Singers</h3>');
+    $(".singers").fadeTo(1000, 1);
+    $(".producers, .remixers, .consultancy").fadeTo(1000, 0.1);
 
-        // Condensed down code to multiple selectors, keeping in below as evidence of previous code.
-        // $('.producers').show();
-        // $('.remixers').show();
-        // $('.consultancy').show();
+    // Condensed down !
+    // $('.remixers').hide(2000);
+    // $('.consultancy').hide(2000);
+  });
 
+  $("#btn-producers").click(function () {
+    $("#hero").removeClass().toggleClass("hero-producers");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Producers</h3>');
+    $(".producers").fadeTo(1000, 1);
+    $(".singers, .remixers, .consultancy").fadeTo(1000, 0.1);
 
-    });
+    // Condensed down !
+    // $('.remixers').hide(2000);
+    // $('.consultancy').hide(2000);
+  });
 
+  $("#btn-remixers").click(function () {
+    $("#hero").removeClass().toggleClass("hero-remixers");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Remixers</h3>');
+    $(".remixers").fadeTo(1000, 1);
+    $(".singers, .producers, .consultancy ").fadeTo(1000, 0.1);
+    // Condensed down !
+    // $('.producers').hide(2000);
+    // $('.consultancy').hide(2000);
+  });
 
-    $('#btn-singers').click(function () {
-        $('#hero').removeClass().toggleClass('hero-singer');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Singers</h3>');
-        $('.singers').fadeTo(1000, 1);
-        $('.producers, .remixers, .consultancy').fadeTo(1000, 0.1);
+  $("#btn-consultancy").click(function () {
+    $("#hero").removeClass().toggleClass("hero-consultancy");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck">Consultancy</h3>');
 
-        // Condensed down !
-        // $('.remixers').hide(2000);
-        // $('.consultancy').hide(2000);
+    $(".consultancy").fadeTo(1000, 1);
+    $(".singers, .producers, .remixers").fadeTo(1000, 0.1);
+    // Condensed down !
+    // $('.producers').hide(2000);
+    // $('.remixers').hide(2000);
+  });
 
+  $("#btn-map").click(function () {
+    $("#map").toggle(1000, 0);
+  });
 
-    });
+  // Mouse fade events - changing hero section and more info display on cards, going to test my knowledge, adding an active state to enable or disable mouse enter...wish me luck! better commit lol!
 
+  $(".singers").hover(function () {
+    $("#hero").removeClass().toggleClass("hero-singer");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Singers</h3>');
+  });
 
+  $(".producers").hover(function () {
+    $("#hero").removeClass().toggleClass("hero-producers");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Producers</h3>');
+  });
 
-    $('#btn-producers').click(function () {
-        $('#hero').removeClass().toggleClass('hero-producers');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Producers</h3>');
-        $('.producers').fadeTo(1000, 1);
-        $('.singers, .remixers, .consultancy').fadeTo(1000, 0.1);
+  $(".remixers").hover(function () {
+    $("#hero").removeClass().toggleClass("hero-remixers");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck" >Remixers</h3>');
+  });
 
-        // Condensed down !
-        // $('.remixers').hide(2000);
-        // $('.consultancy').hide(2000);
+  $(".consultancy").hover(function () {
+    $("#hero").removeClass().toggleClass("hero-consultancy");
+    $(".hero-text p")
+      .empty()
+      .append('<br><h3 class="focus-in-contract-bck">Consultancy</h3>');
+  });
 
+  // //mouse leave
 
-    });
+  // $('.singers').mouseleave(function(){
+  //     $('#hero').removeClass().toggleClass('hero-hydra');
+  // $('.hero-text p').empty();
 
-    $('#btn-remixers').click(function () {
-        $('#hero').removeClass().toggleClass('hero-remixers');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Remixers</h3>');
-        $('.remixers').fadeTo(1000, 1);
-        $('.singers, .producers, .consultancy ').fadeTo(1000, 0.1);
-        // Condensed down !
-        // $('.producers').hide(2000);
-        // $('.consultancy').hide(2000);
+  // });
 
+  // $('.producers').mouseleave(function(){
+  //     $('#hero').removeClass().toggleClass('hero-hydra');
+  // $('.hero-text p').empty()
+  // });
 
-    });
+  // $('.remixers').mouseleave(function(){
+  //     $('#hero').removeClass().toggleClass('hero-hydra');
+  // $('.hero-text p').empty()
+  // });
 
-    $('#btn-consultancy').click(function () {
-        $('#hero').removeClass().toggleClass('hero-consultancy');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck">Consultancy</h3>');
-
-        $('.consultancy').fadeTo(1000, 1);
-        $('.singers, .producers, .remixers').fadeTo(1000, 0.1);
-        // Condensed down !
-        // $('.producers').hide(2000);
-        // $('.remixers').hide(2000);
-
-
-
-    });
-
-
-    $('#btn-map').click(function () {
-        $('#map').toggle(1000,0);
-    });
-
-    // Mouse fade events - changing hero section and more info display on cards, going to test my knowledge, adding an active state to enable or disable mouse enter...wish me luck! better commit lol!
-
-    $('.singers').hover(function () {
-        $('#hero').removeClass().toggleClass('hero-singer');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Singers</h3>');
-
-    });
-
-
-
-    $('.producers').hover(function () {
-        $('#hero').removeClass().toggleClass('hero-producers');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Producers</h3>');
-    });
-
-    $('.remixers').hover(function () {
-        $('#hero').removeClass().toggleClass('hero-remixers');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck" >Remixers</h3>');
-    });
-
-    $('.consultancy').hover(function () {
-        $('#hero').removeClass().toggleClass('hero-consultancy');
-        $('.hero-text p').empty().append('<br><h3 class="focus-in-contract-bck">Consultancy</h3>');
-    });
-
-
-    // //mouse leave 
-
-    // $('.singers').mouseleave(function(){
-    //     $('#hero').removeClass().toggleClass('hero-hydra');
-    // $('.hero-text p').empty();
-
-    // });
-
-    // $('.producers').mouseleave(function(){
-    //     $('#hero').removeClass().toggleClass('hero-hydra');
-    // $('.hero-text p').empty()
-    // });
-
-    // $('.remixers').mouseleave(function(){
-    //     $('#hero').removeClass().toggleClass('hero-hydra');
-    // $('.hero-text p').empty()
-    // });
-
-    // $('.consultancy').mouseleave(function(){
-    //     $('#hero').removeClass().toggleClass('hero-hydra');
-    // $('.hero-text p').empty()
-    // });
-
-
+  // $('.consultancy').mouseleave(function(){
+  //     $('#hero').removeClass().toggleClass('hero-hydra');
+  // $('.hero-text p').empty()
+  // });
 });
