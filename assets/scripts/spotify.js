944b6109936c65a286fc1fc0bbb12a8ef51058ab
Dan Boots <thestereojackers@gmail.com>
2020-09-18 14:31:45 +0000

    json from spotify downloaed and hard coded as token request will run out, this will be highlighted in readme

---
+++ assets/scripts/spotify.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+console.log('ready for spotify captain')
+
+$.getJSON('https://danielboots.github.io/HydraExplorer/assets/data/ksyran.json', function(data){
+    console.log(data);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+// spotify:artist:2yQLqQ3PH8bsVSeuoFeykL
