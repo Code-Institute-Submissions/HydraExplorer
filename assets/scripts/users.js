fb36d390d77b8ddedaebb5d4d90a20cf303d2537
Dan Boots <thestereojackers@gmail.com>
2020-09-21 13:54:16 +0000

    refresh gitpod commit as something wasnt updating, creating ids for JSON to link to users.html using handlebars to create dynamic user.html pages

--- assets/scripts/users.js
+++ assets/scripts/users.js
@@ -1,7 +1,11 @@
+
 //Code from Handlebars.js tutorial for ref: https://www.youtube.com/watch?v=wSNa5b1mS5Y
 
 /*hardcoded JSON Data ... written by myself, i will use this as a replacement to user input data as  i dont know how to use databases yet, and will increase
 functionality to the members profile by using external API from Spotify to create a dynamic user profile page this can be found in scripts, users.JSON file */
+
+
+
 
 let ourRequest = new XMLHttpRequest();
 
@@ -9,7 +13,7 @@ ourRequest.open("GET", "https://danielboots.github.io/HydraExplorer/assets/data/
 ourRequest.onload = function () {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     let data = JSON.parse(ourRequest.responseText);
-    console.log("Users arrays grafting captain! ready to roll");
+    console.log("A OK El Capitan!");
     createHTML(data);
   } else { 
     console.log("we connected to the server but it returned an error");
@@ -24,14 +28,14 @@ ourRequest.send();
 
 // Handlebars.js - template function
 
-// function createHTML(hydraUserData) {
-//   let rawTemplate = document.getElementById("userCardTemplate").innerHTML;
-//   let compiledTemplate = Handlebars.compile(rawTemplate);
-//   let ourGeneratedHTML = compiledTemplate(hydraUserData);
+function createHTML(hydraUserData) {
+  let rawTemplate = document.getElementById("userCardTemplate").innerHTML;
+  let compiledTemplate = Handlebars.compile(rawTemplate);
+  let ourGeneratedHTML = compiledTemplate(hydraUserData);
 
-//   let userContainer = document.getElementById("user-card-container");
-//   userContainer.innerHTML = ourGeneratedHTML;
+  let userContainer = document.getElementById("user-profile");
+  userContainer.innerHTML = ourGeneratedHTML;
 
-//   console.log(hydraUserData);
-// } 
+  console.log(hydraUserData);
+} 
 
