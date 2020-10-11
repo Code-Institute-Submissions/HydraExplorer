1513e9f8db5d011ddfc2ec82a76d2ff17ff29ce1
Dan Boots <thestereojackers@gmail.com>
2020-10-11 10:19:15 +0000

    fav icon img fixed from console error

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,14 @@
 // tutorial video from https://youtu.be/qoN6-rWIYrk?list=PLtV5RF44Yj8S4RcpQehL-2XMuVsJXwNvK to drill in to data, selecting a user gives page id to display data from the JSON index we need
 
 function getParameterByName(name, url) {
-    if (!url) url=window.location.href;
+    if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
     let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-        results = regex.exec(url);
-        if(!results) return null;
-        if(!results) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-} 
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 let userId = getParameterByName('id');
 
@@ -17,7 +17,7 @@ let userId = getParameterByName('id');
 /*hardcoded JSON Data ... written by myself, i will use this as a replacement to user input data as  i dont know how to use databases yet, and will increase
 functionality to the members profile by using external API from Spotify to create a dynamic user profile page this can be found in scripts, users.JSON file */
 
-  
+
 
 
 
@@ -25,22 +25,22 @@ let ourRequest = new XMLHttpRequest();
 
 ourRequest.open("GET", "https://danielboots.github.io/HydraExplorer/assets/data/users.json");
 ourRequest.onload = function (hydraUserData) {
-  if (ourRequest.status >= 200 && ourRequest.status < 400) {
-    let data = JSON.parse(ourRequest.responseText);
-    console.log("success from server loading users.json (message from main.js script!) ");
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        let data = JSON.parse(ourRequest.responseText);
+        console.log("success from server loading users.json (message from main.js script!) ");
 
-  console.log('data recieved from json', data);
-  console.log("User identification for selected is : ", userId);
-  
-    createHTML(data);
-    return hydraUserData; 
-    
-  } else {  
-    console.log("we connected to the server but it returned an error");
-  }
+        console.log('data recieved from json', data);
+        console.log("User identification for selected is : ", userId);
+
+        createHTML(data);
+        return hydraUserData;
+
+    } else {
+        console.log("we connected to the server but it returned an error");
+    }
 };
 ourRequest.onerror = function () {
-  console.log("connection error");
+    console.log("connection error");
 };
 
 ourRequest.send();
@@ -50,11 +50,11 @@ ourRequest.send();
 /* Handlebars.js - template function, my thoughts.. If has class run this, if not run original function, hydraUserData.users[userId]); */
 
 function createHTML(hydraUserData) {
-  let rawTemplate = document.getElementById("userCardTemplate").innerHTML;
-  let compiledTemplate = Handlebars.compile(rawTemplate);
-  let ourGeneratedHTML = compiledTemplate(hydraUserData);
+    let rawTemplate = document.getElementById("userCardTemplate").innerHTML;
+    let compiledTemplate = Handlebars.compile(rawTemplate);
+    let ourGeneratedHTML = compiledTemplate(hydraUserData);
 
-  if ($('body').hasClass('user-details')) {
+    if ($('body').hasClass('user-details')) {
 
         console.log('does the page have the class, user-details ? query returns true thus this is the users.html page!');
         console.log('selected user from index.html', hydraUserData.users[userId]);
@@ -65,15 +65,15 @@ function createHTML(hydraUserData) {
         console.log(hydraUserData.users[userId].image);
 
 
-// check code with tutor to change css property ... ammendment, figured it out, by chance!
+        // check code with tutor to change css property ... ammendment, figured it out, by chance!
         // let hydraDataHero = $('hydraUserData.users[userId].image');
-        
+
         // correct data background hero and body sections
         $("#hydra-data-hero").css("background-image", "url(" + hydraUserData.users[userId].image + ")");
 
         $(".bg").css("background-image", "url(" + hydraUserData.users[userId].imagebg + ")");
         console.log(hydraUserData.users[userId].imagebg);
-       
+
 
 
         let hydraDataName = $('#hydra-data-name');
@@ -106,35 +106,47 @@ function createHTML(hydraUserData) {
 
 
         //https://stackoverflow.com/questions/179713/how-to-change-the-href-for-a-hyperlink-using-jquery
-    
-        let hydraDataFacebook = $('#hydra-data-facebook');
-        hydraDataFacebook.attr('href', hydraUserData.users[userId].facebook);
 
+
+        //This doesnt seem to be working just yet... check with mentor if cant figure solution myself.
+
+        $('#hydra-data-facebook').empty().attr('href', hydraUserData.users[userId].facebook);
+
+        $('#hydra-data-twitter').empty().attr('href', hydraUserData.users[userId].twitter);
+
+        $('#hydra-data-instagram').empty().attr('href', hydraUserData.users[userId].instagram);
+
+        $('#hydra-data-spotify').empty().attr('href', hydraUserData.users[userId].spotify);
+
+        // check to see if user social handles print out correct data for testing purpose
+        console.log(hydraUserData.users[userId].facebook);
         console.log(hydraUserData.users[userId].twitter);
-        console.log(hydraUserData.users[userId].name);
+        console.log(hydraUserData.users[userId].instagram);
+        console.log(hydraUserData.users[userId].spotify);
+
 
 
 
         // Gallery jQuery - grabbing images from JSON gallery to display per user.
 
-        let hydraDatag1 = $('#hydra-data-g1'); 
+        let hydraDatag1 = $('#hydra-data-g1');
         hydraDatag1.empty().attr('src', hydraUserData.users[userId].gallery[0]);
 
-        let hydraDatag2 = $('#hydra-data-g2'); 
+        let hydraDatag2 = $('#hydra-data-g2');
         hydraDatag2.empty().attr('src', hydraUserData.users[userId].gallery[1]);
 
-        let hydraDatag3 = $('#hydra-data-g3'); 
+        let hydraDatag3 = $('#hydra-data-g3');
         hydraDatag3.empty().attr('src', hydraUserData.users[userId].gallery[2]);
 
 
-        let hydraDatag4 = $('#hydra-data-g4'); 
+        let hydraDatag4 = $('#hydra-data-g4');
         hydraDatag4.empty().attr('src', hydraUserData.users[userId].gallery[3]);
 
 
-        let hydraDatag5 = $('#hydra-data-g5'); 
+        let hydraDatag5 = $('#hydra-data-g5');
         hydraDatag5.empty().attr('src', hydraUserData.users[userId].gallery[4]);
 
-        let hydraDatag6 = $('#hydra-data-g6'); 
+        let hydraDatag6 = $('#hydra-data-g6');
         hydraDatag6.empty().attr('src', hydraUserData.users[userId].gallery[5]);
 
         // console.log(hydraUserData.users[userId].gallery[0]);
@@ -146,18 +158,18 @@ function createHTML(hydraUserData) {
 
 
 
-        
 
-} else {
-    console.log('does the page have the class, user-details ? query returns false thus is the index.html page!');
-   /* moved below let = usercontainer in to this scope and else statement to avoid console.log error 'not being declared' this is true as it is not needed in the users.html page only index.html */
 
-    let userContainer = document.getElementById("user-card-container");
-  userContainer.innerHTML = ourGeneratedHTML;
+    } else {
+        console.log('does the page have the class, user-details ? query returns false thus is the index.html page!');
+        /* moved below let = usercontainer in to this scope and else statement to avoid console.log error 'not being declared' this is true as it is not needed in the users.html page only index.html */
+
+        let userContainer = document.getElementById("user-card-container");
+        userContainer.innerHTML = ourGeneratedHTML;
+    }
+
+
+
+
 }
-
-
-  
-
-} 
 
